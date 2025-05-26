381a8d58c864937056215b13a69b9d698bdf683c
Patrick Bell <patrick@Patricks-Laptop.local>
2025-05-26 14:12:38 +0100

    Initial commit

---
+++ config/initializers/cors.rb
@@ -0,0 +1,19 @@
+ Rails.application.config.middleware.insert_before 0, Rack::Cors do
+   allow do
+     origins "http://localhost:5173"
+
+     resource "*",
+       headers: :any,
+       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+       credentials: true
+   end
+
+   allow do
+    origins "https://minifigs-mania-47c93479337f.herokuapp.com"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+end
